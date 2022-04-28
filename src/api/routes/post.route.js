d1d23b6d20e6a51eb1ca7c3e946c23185b4ea0f3
Eyfie <thomas.mendezdevigo@gmail.com>
2022-04-28 21:25:02 +0200

    Added post.route.js

---
+++ src/api/routes/post.route.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const validation = require('../middlewares/validation');
+const post = require('../controllers/post.controller');
+const multer = require('../middlewares/multer');
+const { postSchema } = require('../validations/post.validation');
+
+router.get('/post', post.getAllPosts);
+router.get('/:postId', post.getPost);
+router.post('/post', (...args) => validation(postSchema, ...args), multer, post.createPost);
+router.patch('/:postId', (...args) => validation(postSchema, ...args), multer, post.modifyPost);
+router.delete('/:postId/delete', post.deletePost);
