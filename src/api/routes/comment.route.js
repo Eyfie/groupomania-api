bf0ea3a42714f37befe17be2520466bf430aaf2c
Eyfie <thomas.mendezdevigo@gmail.com>
2022-04-29 15:47:04 +0200

    Added comment.route.js

---
+++ src/api/routes/comment.route.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const validation = require('../middlewares/validation');
+const comment = require('../controllers/comment.controller');
+const multer = require('../middlewares/multer');
+const { commentSchema } = require('../validations/comment.validation');
+
+router.get('/', comment.getAllComments);
+router.get('/:commentId', comment.getComment);
+router.post('/', (...args) => validation(commentSchema, ...args), multer, comment.createComment);
+router.patch('/:commentId', (...args) => validation(commentSchema, ...args), multer, comment.modifyComment);
+router.delete('/:commentId', comment.deleteComment);
+
+module.exports = router;
