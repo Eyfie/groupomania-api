ffb510598c83946a47c2d1aee73eb0eec47ac799
Eyfie <thomas.mendezdevigo@gmail.com>
2022-04-29 15:56:32 +0200

    Added comment.controler.js

---
+++ src/api/controllers/comment.controller.js
@@ -0,0 +1,38 @@
+const createError = require('http-errors');
+const commentService = require('../services/comment.service');
+
+exports.getAllComments = async (req, res, next) => {
+  try {
+    
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getComment = async (req, res, next) => {
+  try {
+    
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.createComment = async (req, res, next) => {
+  try {
+    
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.gmodifyComment = async (req, res, next) => {
+  try {
+    
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.deleteComment = async (req, res, next) => {
+
+};
