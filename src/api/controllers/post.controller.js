1f31fd5156de6caa19b969f6bda1094f7094baa6
Eyfie <thomas.mendezdevigo@gmail.com>
2022-04-30 12:23:43 +0200

    Removed Errors 500 from controller

--- src/api/controllers/post.controller.js
+++ src/api/controllers/post.controller.js
@@ -1,13 +1,11 @@
 /* eslint-disable max-len */
 /* eslint-disable object-curly-newline */
-const createError = require('http-errors');
 const postService = require('../services/post.service');
 
 exports.getAllPosts = async (req, res, next) => {
   const { accessToken } = req;
   try {
     const allPosts = await postService.getAllPosts(accessToken);
-    if (!allPosts) throw new createError[500]('Something went wrong, please try again (controller)');
     res.status(200).json({ message: 'Posts found' }, ...allPosts);
   } catch (error) {
     next(error);
@@ -18,7 +16,6 @@ exports.getPost = async (req, res, next) => {
   const { params, accessToken } = req;
   try {
     const post = await postService.getPost(params, accessToken);
-    if (!post) throw new createError[500]('Something went wrong, please try again (controller)');
     res.status(200).json({ message: 'Post found' }, ...post);
   } catch (error) {
     next(error);
@@ -30,7 +27,6 @@ exports.createPost = async (req, res, next) => {
   const host = req.get('host');
   try {
     const createdPost = await postService.createPost(body, file, protocol, accessToken, host);
-    if (!createdPost) throw new createError[500]('Something went wrong, please try again (controller)');
     res.status(201).json({ message: 'Post created' }, ...createdPost);
   } catch (error) {
     next(error);
@@ -42,7 +38,6 @@ exports.modifyPost = async (req, res, next) => {
   const host = req.get('host');
   try {
     const updatedPost = await postService.modifyPost(params, body, file, protocol, host, accessToken);
-    if (!updatedPost) throw new createError[500]('Something went wrong, please try again (controller)');
     res.status(200).json({ message: 'Post updated' }, ...updatedPost);
   } catch (error) {
     next(error);
@@ -53,7 +48,6 @@ exports.deletePost = async (req, res, next) => {
   const { params, accessToken } = req;
   try {
     const deletedPost = await postService.deletePost(params, accessToken);
-    if (!deletedPost) throw new createError[500]('Something went wrong, please try again (controller)');
     res.status(200).json({ message: 'Post deleted' }, ...deletedPost);
   } catch (error) {
     next(error);
