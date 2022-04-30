f0e744636bb45dcf2f9e2cbe59ba8497e28e8f5f
Eyfie <thomas.mendezdevigo@gmail.com>
2022-04-30 23:22:45 +0200

    Fixed post routes

--- src/api/routes/post.route.js
+++ src/api/routes/post.route.js
@@ -4,9 +4,9 @@ const post = require('../controllers/post.controller');
 const multer = require('../middlewares/multer');
 const { postSchema } = require('../validations/post.validation');
 
-router.get('/post', post.getAllPosts);
+router.get('/', post.getAllPosts);
 router.get('/:postId', post.getPost);
-router.post('/post', (...args) => validation(postSchema, ...args), multer, post.createPost);
+router.post('/', (...args) => validation(postSchema, ...args), multer, post.createPost);
 router.patch('/:postId', (...args) => validation(postSchema, ...args), multer, post.modifyPost);
 router.delete('/:postId', post.deletePost);
 
