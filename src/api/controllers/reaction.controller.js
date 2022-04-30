38b7f76dab7732e34a9dcb356a75e422b8aded59
Eyfie <thomas.mendezdevigo@gmail.com>
2022-04-30 12:54:15 +0200

    Added reaction.controller.js

---
+++ src/api/controllers/reaction.controller.js
@@ -0,0 +1,41 @@
+const reactionService = require('../services/reaction.service');
+
+exports.geAllReactions = async (req, res, next) => {
+  const { accessToken } = req;
+  try {
+    const allReactions = await reactionService.getAllReactions(accessToken);
+    res.status(200).json({ message: 'Reactions found' }, ...allReactions);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.createReaction = async (req, res, next) => {
+  const { body, accessToken } = req;
+  try {
+    const newReaction = await reactionService.createReaction(body, accessToken);
+    res.status(201).json({ message: 'Reaction created' }, ...newReaction);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.modifyReaction = async (req, res, next) => {
+  const { params, body, accessToken } = req;
+  try {
+    const updatedReaction = await reactionService.modifyReaction(params, body, accessToken);
+    res.status(200).json({ message: 'Reaction updated' }, ...updatedReaction);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.deleteReaction = async (req, res, next) => {
+  const { params, accessToken } = req;
+  try {
+    const deletedReaction = await reactionService.deleteReaction(params, accessToken);
+    res.status({ message: 'Reaction deleted' }, ...deletedReaction);
+  } catch (error) {
+    next(error);
+  }
+};
