c3a05a5f3754b3866920986291830596d3b4b351
Eyfie <thomas.mendezdevigo@gmail.com>
2022-05-03 11:10:19 +0200

    Added right checker middleware

---
+++ src/api/middlewares/rights.js
@@ -0,0 +1,19 @@
+const createError = require('http-errors');
+const  { User } = require('../models/User');
+
+const rights = async (req, res, next) => {
+  const { accessToken } = req;
+  try {
+    const UserId = accessToken.user.id;
+    const userFound = await User.findOne({ where: { id: UserId } });
+    if (!userFound) throw new createError[404]('User not found');
+
+    if (userFound.role !== 'moderator') throw new createError[401]('Not Authorized');
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = rights;
