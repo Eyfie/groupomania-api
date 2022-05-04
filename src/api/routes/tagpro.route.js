c7dbe9660f026bf42fc1cf2d54a10f720f654338
Eyfie <thomas.mendezdevigo@gmail.com>
2022-05-04 11:18:30 +0200

    Added tagpro route

---
+++ src/api/routes/tagpro.route.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const validation = require('../middlewares/validation');
+const rights = require('../middlewares/rights');
+const tagpro = require('../controllers/tagpro.controller');
+const { tagproSchema } = require('../validations/tagpro.validation');
+
+router.get('/', tagpro.getAllTagpros);
+router.get('/:tagproId', tagpro.getTagpro);
+router.post('/', rights, (...args) => validation(tagproSchema, ...args), tagpro.createTagpro);
+router.patch('/:tagproId', rights, (...args) => validation(tagproSchema, ...args), tagpro.modifyTagpro);
+router.delete('/:tagproId', rights, tagpro.deleteTagpro);
+
+module.exports = router;
