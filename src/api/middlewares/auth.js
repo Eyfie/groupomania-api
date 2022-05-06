1adc27490b5bdf24996fb5b9c27f411cb89fc372
Eyfie <thomas.mendezdevigo@gmail.com>
2022-05-06 14:25:35 +0200

    Updated auth with User verification

--- src/api/middlewares/auth.js
+++ src/api/middlewares/auth.js
@@ -1,12 +1,21 @@
 const createError = require('http-errors');
 const { verifyAccessToken } = require('../helpers/token');
+const { User } = require('../models');
 
-const auth = (req, res, next) => {
+const auth = async (req, res, next) => {
   try {
+    if (!req.headers.authorization) throw new createError[401]('No authorizations');
+
     const accessToken = req.headers.authorization.split(' ')[1];
     if (!accessToken) throw new createError[401]('You need to be logged in');
 
     req.accessToken = verifyAccessToken(accessToken);
+
+    const UserId = req.accessToken.user.id;
+    const userFound = await User.findOne({ where: { id: UserId } });
+    if (!userFound) throw new createError[404]('User not found');
+
+    req.accessToken.user = userFound;
 
     next();
   } catch (error) {
