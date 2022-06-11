51a5c68b2f68caade01a2e88238daf661e943784
Eyfie <thomas.mendezdevigo@gmail.com>
2022-06-11 02:30:02 +0200

    Updated auth middleware

--- src/api/middlewares/auth.js
+++ src/api/middlewares/auth.js
@@ -7,15 +7,14 @@ const auth = async (req, res, next) => {
     if (!req.headers.authorization) throw new createError[401]('No authorizations');
 
     const accessToken = req.headers.authorization.split(' ')[1];
-    if (!accessToken) throw new createError[401]('You need to be logged in');
+    if (!accessToken) throw new createError[401]('Not logged in');
 
     req.accessToken = verifyAccessToken(accessToken);
-
     const UserId = req.accessToken.user.id;
     const userFound = await User.findOne({ where: { id: UserId } });
     if (!userFound) throw new createError[404]('User not found');
 
-    req.accessToken.user = userFound;
+    req.accessToken.user = userFound.dataValues;
 
     next();
   } catch (error) {
