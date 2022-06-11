4cc1406818bdd79338e1ade17ca921b0cf4c8d66
Eyfie <thomas.mendezdevigo@gmail.com>
2022-06-11 02:31:04 +0200

    Updated rights handler middleware

--- src/api/middlewares/rights.js
+++ src/api/middlewares/rights.js
@@ -1,15 +1,9 @@
 const createError = require('http-errors');
-const { User } = require('../models');
 
 const rights = async (req, res, next) => {
   const { accessToken } = req;
   try {
-    const UserId = accessToken.user.id;
-    const userFound = await User.findOne({ where: { id: UserId } });
-    if (!userFound) throw new createError[404]('User not found');
-
-    if (userFound.role !== 'moderator') throw new createError[401]('Not Authorized');
-
+    if (accessToken.user.role !== 'moderator') throw new createError[401]('Not Authorized');
     next();
   } catch (error) {
     next(error);
